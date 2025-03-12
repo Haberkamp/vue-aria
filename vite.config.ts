4e7c97377265524566414d2e6a0312b31bffdda5
Nils Haberkamp <haberkamp.n@gmail.com>
2025-03-12 22:14:22 +0100

    build: enable library mode for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(import.meta.dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"], // we only support ESM
+    },
+  },
+});
