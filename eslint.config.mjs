a05877343ef283fe46d943b9acf8888a3153a2eb
Nils Haberkamp <haberkamp.n@gmail.com>
2025-03-14 13:14:01 +0100

    build: add linting rules for tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,22 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import vitest from "eslint-plugin-vitest";
 
 export default tseslint.config(
   {
     ignores: ["docs/**"],
   },
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.test.{ts,tsx,js,jsx}"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
+      "vitest/max-nested-describe": ["error", { max: 3 }], // you can also modify rules' behavior using option like this
+    },
+  }
 );
